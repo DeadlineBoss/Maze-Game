41d7e6ad97198595473f1f1578d9f272372d95b6
DeadlineBoss <tricksylife@gmail.com>
2021-01-12 16:18:15 +0530

    added player

--- sketch.js
+++ sketch.js
@@ -3,10 +3,12 @@ var player;
 function setup() {
     //This runs once
     createCanvas(400,400);
-    
+
+    player = createSprite(200,200,10,10);
 }
 
 function draw() {
     //This runs again and again
 
+    drawSprites();
 }
